e89c79a242689f1d3b8546f2c2df53e4766954c0
Jonathan San Miguel <guerreroz80@outlook.es>
2022-10-31 08:54:41 -0600

    KiotoV1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/Items'
+  },
+  {
+  path: 'Items',
+  component: ItemListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
